b25f864c1a21bc1977b83f8deec2f5bba2de7c9d
Valkyrie Savage <valkyrie@eecs.berkeley.edu>
2012-04-18 14:18:20 -0700

    possibly now modified to run on entire dataset

--- strong_features.m
+++ strong_features.m
@@ -27,24 +27,49 @@ end
 % return pyramid descriptors for all files in filenames
 pyramid_test = BuildPyramid(filenames,test_image_dir,data_dir);
 
-wonz = ones(142);
-zeroze = zeros(3001-142);
-testClass = [wonz(1,:), zeroze(1,:)]';
+% compute histogram intersection kernel
+K = [(1:num_train_files)' , hist_isect(pyramid_train, pyramid_train)]; 
+KK = [(1:num_test_files)' , hist_isect(pyramid_test, pyramid_train)];
 
-%for(i=1:15)
+test_class_counts = [141, 260, 228, 160, 208, 274, 310, 192, 256, 115, 116, 211, 110, 189, 215];
+test_classes = [];
+for i=1:15
+    truez = ones(test_class_counts(i))*i;
+    test_classes = vertcat(test_classes, truez(:,1));
+end
+
+decision_values = [];
+
+for i=1:15
+    pre_zeroze = zeros((i-1)*100);
     wonz = ones(100);
-    zeroze = zeros(1400);
-    trainClass = [wonz(1,:), zeroze(1,:)]';
-    % compute histogram intersection kernel
-    K = [(1:num_train_files)' , hist_isect(pyramid_train, pyramid_train)]; 
-    KK = [(1:num_test_files)' , hist_isect(pyramid_test, pyramid_train)];
-
+    post_zeroze = zeros(1400);
+    if(i > i)
+        train_class = [pre_zeroze(1,:), wonz(1,:), post_zeroze(1,:)]';
+    else
+        train_class = [wonz(1,:), post_zeroze(1,:)]';
+    end
+    
+    pre_zeroze = zeros(sum(test_class_counts(1:i)));
+    wonz = ones(test_class_counts(i));
+    post_zeroze = zeros(sum(test_class_counts(i+1:length(test_class_counts))));
+    if(i > 1)
+        test_class = [pre_zeroze(1,:), wonz(1,:), post_zeroze(1,:)]';
+    else
+        test_class = [wonz(1,:), post_zeroze(1,:)]';
+    end
 
     %# train and test
-    model = svmtrain(trainClass, K, '-t 4');
-    [predClass, acc, decVals] = svmpredict(testClass, KK, model);
-%end
+    model = svmtrain(train_class, K, '-t 4');
+    [predicted_class, accuracy, decision_value] = svmpredict(test_class, KK, model);
+    decision_values(:,i) = decision_value;
+end
 
+ultimate_decisions = [];
+for i=1:num_test_files
+    [value, idx] = max(decision_values(i,:));
+    ultimate_decisions(i) = idx;
+end
 
 %# confusion matrix
-C = confusionmat(testClass,predClass)
+C = confusionmat(test_classes, ultimate_decisions')
