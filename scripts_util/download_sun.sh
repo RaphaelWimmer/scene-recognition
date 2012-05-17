5c91293356649ec7a0ead4bfcf8ea687163c27bf
Shiry Ginosar <shiry@psi.Millennium.Berkeley.EDU>
2012-05-17 13:02:38 -0700

    script to download sun onto cluster

---
+++ scripts_util/download_sun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget http://groups.csail.mit.edu/vision/SUN1old/SUN397.tar -o /work4/shiry/scene-recognition/data/sun/log -P /work4/shiry/scene-recognition/data/sun/
+tar xvf /work4/shiry/scene-recognition/data/sun/SUN397.tar -C /work4/shiry/scene-recognition/data/sun/
